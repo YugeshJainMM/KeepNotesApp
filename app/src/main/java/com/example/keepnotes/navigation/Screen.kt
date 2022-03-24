e84d453a6f5d06f6cf1bc28d98e9d2955b4ea9b4
Yugesh Jain <yugesh.jain@mutualmobile.com>
2022-03-24 12:57:32 +0530

    NavGraph removed, Compose Destinations added

--- app/src/main/java/com/example/keepnotes/navigation/Screen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.keepnotes.navigation
-
-//sealed class Screen(val route: String) {
-//    object LoginScreen : Screen("login_screen")
-//    object RegisterationScreen : Screen("registration_screen")
-//    object MainScreen : Screen("main_screen")
-//    object ForgotPasswordScreen : Screen("forgot_password_screen")
-//
-//}
